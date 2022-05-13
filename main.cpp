cae6e18b75e38ac490bea5d7f7019442ce70355d
raytective <rkwatana@hawaii.edu>
2022-05-13 10:19:38 -1000

    Caught exceptions for width/length <=0 :muscle:

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Rachel Watanabe <rkwatana@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include "Square.h"
+#include <assert.h>
+#define DDEBUG
 
 int main() {
+#ifdef DEBUG
+    /// testing rectangle (0 width/length)
+    try{
+        Rectangle sillyRectangle(0,0);
+        assert(false);
+    } catch (std::exception) {
+        std::cout << "Can't have rectange length/width < 0" << std::endl;
+    }
+
+    /// testing square (0 width/length)
+    try{
+        Square sillySquare( -1 );
+        assert(false);
+    } catch( std::exception ) {
+        std::cout << "Can't have square length/width < 0" << std::endl;
+    }
+
+    /// testing getters
+    Square funnySquare( 25 );
+    std::cout << "Side of funny square is " << funnySquare.getSide() << std::endl;
+
+    Rectangle funnyRectangle( 35, 10 );
+    std::cout << "Length of funny rectange is " << funnyRectangle.getLength() << std::endl;
+    std::cout << "Width of funny square is " << funnyRectangle.getWidth() << std::endl;
+#endif
+
     std::cout << "Hello, World!" << std::endl;
 
     Square mySquare(2);
